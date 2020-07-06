df1aa7d26f23c1caf4ccabf74c0142d6bf81614c
adnansadar <adnansadar11@gmail.com>
2020-07-06 12:24:00 +0530

    added onClick functionality to UI

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import "./App.css";
+// import "./App.css";
 import Menu from "./components/MenuComponent";
 import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
   render() {
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Confusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
