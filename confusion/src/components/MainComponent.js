fa6dc923d41432880a125d7ead837f54cec5ea87
adnansadar <adnansadar11@gmail.com>
2020-07-09 10:43:10 +0530

    added React-Router functionality

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,41 +1,44 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
+// import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
-import DishDetail from "./DishdetailComponent";
+// import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null,
     };
   }
-
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
-  }
-
   render() {
+    const HomePage = () => {
+      return <Home />;
+    };
+
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu
-          dishes={this.state.dishes}
-          onClick={(dishId) => this.onDishSelect(dishId)}
-        />
-        <DishDetail
-          dish={
-            this.state.dishes.filter(
-              (dish) => dish.id === this.state.selectedDish
-            )[0]
-          }
-        />
+        <Header />
+        <Switch>
+          {/* If we know that only one route will be chosen, we can use a <Switch> component to render only the first route that matches the location(used for exact path) */}
+          {/* Dynamic Routing used */}
+          {/* <Link> components are how you create links to those different routes */}
+          {/* <Route> component needs a path which is the URL and then a component that you want rendered when navigating to that path. */}
+          <Route path="/home" component={HomePage} />
+          {/*"exact path" only displays one component at a time and switches between the components when the link is pressed */}
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu dishes={this.state.dishes} />}
+          />
+          {/* Redirect= Default if wrong URL entered */}
+          <Redirect to="/home" />
+        </Switch>
+        <Footer />
       </div>
     );
   }
