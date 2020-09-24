a1c1a0b6dfb89454552c74917ca43c29ebd0feaf
adnansadar <adnansadar11@gmail.com>
2020-09-24 20:40:24 +0530

    typicode api

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,9 @@
-export const baseUrl = "http://localhost:3001/";
+let baseUrl;
+if (process.env.NODE_ENV === "production") {
+  baseUrl =
+    "https://my-json-server.typicode.com/adnansadar/restaurant-json-server/";
+} else {
+  baseUrl = "http://localhost:3001/";
+}
+
+export { baseUrl };
